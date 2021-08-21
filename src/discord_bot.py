3ce97650a5c4c9ae138259ba5dee75d31db158db
Ciarán Mulholland <ciaranmul@googlemail.com>
2021-08-21 15:11:44 +0100

    Adding keyword response functionality
    - data file should now be provided for keyword:response dict
    - if a message contains a keyword the bot will now send the appropriate response- refactoring to pull code out of `on_message`

--- src/discord_bot.py
+++ src/discord_bot.py
@@ -1,8 +1,9 @@
 import discord
 
-from utils import get_env, get_random_quote
+from utils import get_env, get_random_quote, get_keywords_dict
 
 client = discord.Client()
+keywords_dict = get_keywords_dict()
 
 @client.event
 async def on_ready():
@@ -13,9 +14,24 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    if await responded_to_keyword(message):
+        return
+
+    if await responded_to_mention(message):
+        return
+
+async def responded_to_keyword(message):
+    for keyword in keywords_dict.keys():
+        if keyword in message.content:
+            await message.channel.send(keywords_dict[keyword])
+            return 1
+    return 0
+
+async def responded_to_mention(message):
     if client.user.mentioned_in(message):
         await message.channel.send(get_random_quote())
-        return
+        return 1
+    return 0
 
 client.run(get_env("API_KEY", __file__))
     
